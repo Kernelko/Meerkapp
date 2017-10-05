621257b46e017eeb7d1b75bdab4c1e6129783fab
Cloudy Fox Panic Basket <disco-cefalizacja@live.com>
2017-10-05 21:24:11 +0000

    added some buttons

--- kernelko.js
+++ kernelko.js
@@ -1,4 +1,5 @@
 
+
 //    $(document).ready(function(){
 //        $("#how_many_pennies").keyup(function(){
   //          let expense = $("#how_many_pennies").val();
@@ -7,13 +8,22 @@
         //});
 function money_in(){
   //    button click
-  document.getElementById("para_content").innerHTML = "Pieniązki otrzymane"
+  document.getElementById("para_content").innerHTML = "Pieniązki otrzymane";
+
 }
 
 function money_out(){
     //    button click
     document.getElementById("para_content").innerHTML = "Pieniążki wydane"
-    
+     	$("#expense_input").removeClass("hidden");
+       
+     let expense_name = $("#expense_name").val();
+     let expense_location=$("#expense_location").val();
+     let expense_value=$("#expense_value").val();
+     let expense_currency=$("#expense_currency").val();
+     let expense_category=$("#expense_category").val();
+     let expense_date=$("#expense_date").val();
+     
   }
 
   function chart_display(){
