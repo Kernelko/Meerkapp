a06a41ed6efa64ce4cab0a4da13e6e0218d2030b
Cloudy Fox Panic Basket <disco-cefalizacja@live.com>
2017-10-07 22:01:30 +0000

    changed idea - one button and then three buttons

--- kernelko.js
+++ kernelko.js
@@ -8,23 +8,16 @@
         //});
 function money_in(){
   //    button click
-  	$("#income_input").removeClass("hidden");
-    $("#expense_input").addClass("hidden");
+  	$("#transfer_container").removeClass("hidden");
 
 }
 
-function money_out(){
-    //    button click
-     	$("#expense_input").removeClass("hidden");
-      $("#income_input").addClass("hidden");
-     let expense_name = $("#expense_name").val();
-     let expense_location=$("#expense_location").val();
-     let expense_value=$("#expense_value").val();
-     let expense_currency=$("#expense_currency").val();
-     let expense_category=$("#expense_category").val();
-     let expense_date=$("#expense_date").val();
-     
-  }
+function hidecontainers(){
+  $("#transfer_container").addClass("hidden");
+  
+}
+
+
 
   function chart_display(){
     //    button click
